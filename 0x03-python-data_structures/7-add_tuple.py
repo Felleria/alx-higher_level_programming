7d69374ab227c65f44ba54d0e0319ad72becdc74
root <tellytabbie@gmail.com>
2022-12-05 23:22:52 +0000

    commits

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-def new_in_list(my_list, idx, element):
-    copy = my_list.copy()
-    if idx < 0 or idx > len(my_list) - 1:
-        return my_list.copy()
-    else:
-        copy[idx] = element
-        return copy
+def add_tuple(tuple_a=(), tuple_b=()):
+    new_tuple = ()
+    tuple_1 = tuple_a + (0, 0)
+    tuple_2 = tuple_b + (0, 0)
+    new_tuple = tuple_1[0] + tuple_2[0], tuple_1[1] + tuple_2[1]
+    return new_tuple
